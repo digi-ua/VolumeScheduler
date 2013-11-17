420d076739a9e5027c1374ce31d74a81949934f6
Bilovus Dmytro <digi.te.ua@gmail.com>
2013-11-17 17:17:47 +0200

    added GetCurrentTime()

--- src/com/example/volumescheduler/MainService.java
+++ src/com/example/volumescheduler/MainService.java
@@ -1,5 +1,6 @@
 package com.example.volumescheduler;
 
+import java.util.Calendar;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -38,13 +39,13 @@ public class MainService extends Service {
 	  
 	  class MainRun implements Runnable {
 		  
-	    int next_time;
-	    int curr_time;
-	    int curr_volume;
+	    Calendar next_time;
+	    Calendar curr_time;
+	    int curr_volume;//type
 	    
 	    public MainRun() {
 	    	Log.d(LOG_TAG, "MainRun create");
-	      //curr_time = GetCurrTime();
+	      GetCurrentTime(curr_time);
 	      //curr_volume = ReadCurrentVolume(curr_time);
 	      //SetCurrentProp(curr_volume);
 	      //next time = ReadNextTime();
@@ -56,7 +57,7 @@ public class MainService extends Service {
 	      try {
 	    	  for(int i = 0;;i++)
 	    	  {
-		    	  //curr_time = GetCurrTime();
+		    	  GetCurrentTime(curr_time);
 		    	  //if(curr_time >= next_time)
 		    	  //{
 		    	  //	curr_volume = ReadCurrentProp();
@@ -75,5 +76,22 @@ public class MainService extends Service {
 	      stopSelf();
 	    }
 	    */
+	    
+	    private void GetCurrentTime(Calendar cl)
+	    {
+	    	cl = Calendar.getInstance(); 
+	    	/*
+	    	int millisecond = cl.get(Calendar.MILLISECOND);
+	    	int second = cl.get(Calendar.SECOND);
+	    	int minute = cl.get(Calendar.MINUTE);//12 hour format	    	      
+	    	int hour = cl.get(Calendar.HOUR);	    	     
+	    	int hourofday = cl.get(Calendar.HOUR_OF_DAY);//24 hour format
+	    	int dayofyear = cl.get(Calendar.DAY_OF_YEAR);
+	    	int year = cl.get(Calendar.YEAR);
+	    	int dayofweek = cl.get(Calendar.DAY_OF_WEEK);
+	    	int dayofmonth = cl.get(Calendar.DAY_OF_MONTH);
+	    	*/
+	    }
+	    
 	  }	
 }
