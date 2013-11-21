50b04e1cd86151436d7538285a06a1108c1f6345
chuhrainka <chuhrainka@gmail.com>
2013-11-21 14:30:20 +0200

    Edit

--- src/com/example/volumescheduler/Manager.java
+++ src/com/example/volumescheduler/Manager.java
@@ -1,5 +1,12 @@
 package com.example.volumescheduler;
 
 public class Manager {
-	
+	DBHelper db = new DBHelper(MainService.this);
+	public void Verification(TimeTable tt)
+	{
+		if(tt.id == 0)
+		{
+			
+		}
+	}
 }
