76c52620b0c57e44cfeb363e425ba989b2240c77
Yuriy Protskiv <iquadre@gmail.com>
2013-11-25 14:43:07 +0200

    fixed addTime dialogFragment

--- src/com/example/volumescheduler/AddRule.java
+++ src/com/example/volumescheduler/AddRule.java
@@ -1,26 +1,31 @@
 package com.example.volumescheduler;
 
+import com.example.volumescheduler.AddTime.onSubmitListener;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class AddRule extends Activity {
+public class AddRule extends Activity implements onSubmitListener {
 	final String LOG_TAG = "myLogs";
-	DialogFragment dlg;
+	AddTime dlg;
 	ToggleButton vibrate;
+	Button add_rule;
 	TextView tgl;
+	TextView time;
 
-	private int START_HOUR;
-	private int START_MIN;
-	private int END_HOUR;
-	private int END_MIN;
+	private int S_HOUR;
+	private int S_MIN;
+	private int E_HOUR;
+	private int E_MIN;
 
 	private int T_DAY;
 	private int T_RULE;
@@ -31,10 +36,14 @@ public class AddRule extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_rule);
 
-		dlg = new AddTime();
+		add_rule = (Button) findViewById(R.id.btn_add);
 		vibrate = (ToggleButton) findViewById(R.id.tgl_vibrate);
 		tgl = (TextView) findViewById(R.id.tbx_tgl);
 		tgl.setText("Unchecked");
+		
+		time = (TextView) findViewById(R.id.tbx_time);
+		time.setText("Nothing");
+
 		vibrate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
 			@Override
@@ -49,16 +58,40 @@ public class AddRule extends Activity {
 				}
 			}
 		});
+		/*
+		add_rule.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				dlg = new AddTime();
+				dlg.mListener = AddRule.this;
+				dlg.show(getFragmentManager(), "dlg1");
+			}
+		});
+		*/
 	}
 
+	
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btn_addTime:
+			dlg = new AddTime();
+			dlg.mListener = AddRule.this;
 			dlg.show(getFragmentManager(), "dlg1");
 			break;
 		default:
 			break;
 		}
+	}
+	
+
+	@Override
+	public void setOnSubmitListener(int s_hour, int s_min, int e_hour, int e_min) {
+		S_HOUR = s_hour;
+		S_MIN = s_min;
+		E_HOUR = e_hour;
+		E_MIN = e_min;
+		time.setText(s_hour + ":" + s_min + " - " + e_hour + ":" + e_min);
 	}
 
 }
