22f0c3ee12bb890860b1c45e104f8ed6695a8f5c
chuhrainka <chuhrainka@gmail.com>
2013-11-25 17:45:57 +0200

    Manager Add Method

--- src/com/example/volumescheduler/Manager.java
+++ src/com/example/volumescheduler/Manager.java
@@ -1,28 +1,33 @@
 package com.example.volumescheduler;
 
+import java.util.Dictionary;
 import java.util.List;
+import java.util.Comparator;
 
 
 public class Manager {
+	public static Dictionary GetAllTime(TimeTable[] ttObjects)
+	{
+		for (int i = 0; i < ttObjects.length; i++) {
+			if(ttObjects[i].state != 0)
+			{
+				for (int j = 0; j < ttObjects.length; j++) {
+					int isEqual = compare((int)ttObjects[i].state, (int)ttObjects[j].id);
+					if(isEqual == 0)
+				}
+			}
+		}
+		return null;
+	}
 	/*в статус≥ початку записане ≥д статусу к≥нц€б в статус≥ к≥нц€ записаний нуль*/
 	private boolean CreateUpdateNewRule(TimeTable[] ttObjects)
 	{
 		DBHelper dBHelper = new DBHelper();
 		List<TimeTable> ttList = dBHelper.getAll();
-		try {
-			int j = 0;
-				int tmp = ttObjects[j].state;
-				if(tmp != 0)
-				{
-					
-					for (int i = 0; i < ttObjects.length; i++) {
-						
-					}
-				}
+		try {			
 			for (int i = 0; i < ttObjects.length; i++) {
 				dbHelper.Save(ttObjects[i]);
-			}
-			
+			}			
 		} catch (Exception e) {
 			return false;
 		}
