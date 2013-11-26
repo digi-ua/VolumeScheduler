0cecc79acd172d29d9ee7def4b65d66ba263c44f
chuhrainka <chuhrainka@gmail.com>
2013-11-26 22:46:04 +0200

    Maybe help

--- src/com/example/volumescheduler/Manager.java
+++ src/com/example/volumescheduler/Manager.java
@@ -8,65 +8,24 @@ import java.util.Comparator;
 
 
 public class Manager {
-	public static Dictionary GetAllTime(TimeTable[] ttObjects)
-	{
-		int timeBegin, timeEnd;
-		Dictionary<Integer, Integer> timesDictionary = new Hashtable();
-		for (int i = 0; i < ttObjects.length; i++) {
-			if(ttObjects[i].state != 0)
-			{
-				for (int j = 0; j < ttObjects.length; j++) {
-					if((int)ttObjects[i].state == (int)ttObjects[j].id)
-					{
-						timesDictionary.put(ttObjects[i].id, ttObjects[j].id);
-					}
-				}
-			}
-		}
-		return timesDictionary;
-	}
-	
-	public static Dictionary GetAllTime(List<TimeTable> ttList)
-	{
-		int timeBegin, timeEnd;
-		Dictionary<Integer, Integer> timesDictionary = new Hashtable();
-		for (TimeTable timeTable : ttList) {
-			if(timeTable.state != 0)
-			{
-				for (TimeTable tTable : ttList) {
-					if((int)tTable.state == (int)tTable.id)
-					{
-						timesDictionary.put(tTable.id, tTable.id);
-					}
-				}
-			}
-		}
-		return timesDictionary;
-	}
 	
 	/*в статус≥ початку записане ≥д статусу к≥нц€, в статус≥ к≥нц€ записаний нуль*/
-	private boolean CreateUpdateNewRule(TimeTable[] ttObjects)
+	private boolean CreateUpdateNewRule(TimeTable tt)
 	{
 		DBHelper dBHelper = new DBHelper(null);
 		List<TimeTable> ttList = dBHelper.getAll();
-		Dictionary<Integer, Integer> ruleFromForm = GetAllTime(ttObjects);
-		Dictionary<Integer, Integer> ruleFromDB = GetAllTime(ttList);
 		
-		try {			
-			for (int i = 0; i < ttObjects.length; i++) {
-				dbHelper.Save(ttObjects[i]);
-			}			
+		try {
+				dbHelper.Save(tt);	
 		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
-	private boolean DeleteRule(TimeTable[] ttObjects)
+	private boolean DeleteRule(TimeTable tt)
 	{
 		try {
-			for (int i = 0; i < ttObjects.length; i++) {
-				dBHelper(ttObjects[i].id);
-			}			
+				dBHelper(tt.id);
 		} catch (Exception e) {
 			return false;
 		}
