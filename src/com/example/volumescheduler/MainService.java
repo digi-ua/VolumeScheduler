cdbba1d02c89dd2341b23609344db1f2035a334c
Bilovus Dmytro <digi.te.ua@gmail.com>
2013-11-27 00:21:35 +0200

    maybe workable service

--- src/com/example/volumescheduler/MainService.java
+++ src/com/example/volumescheduler/MainService.java
@@ -1,6 +1,7 @@
 package com.example.volumescheduler;
 
-/*import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -39,24 +40,30 @@ public class MainService extends Service {
             return null;
           }
           
-          /*class MainRun implements Runnable {
+          class MainRun implements Runnable {
 
                 Time next_time;        
                 Time curr_time;
                 int curr_rule;
+                RuleModel currentRuleModel;
                 
                 DBHelper db = null;
-                List<TimeTable> ttList = null;
+                List<RuleModel> ttList = null;
 
             public MainRun() {
                     db = new DBHelper(MainService.this);
                     ttList = db.getAll();
-                    curr_time = GetCurrentTime();           
-                    Log.d(LOG_TAG, "MainService 3");
-                    curr_rule = GetCurrentMode(curr_time);
-                    SetRule(curr_rule);
-                    next_time = GetNextTime(curr_time);  
-                    Log.d(LOG_TAG, "MainService 6");
+                    curr_time = GetCurrentTime();
+                    
+                    currentRuleModel = GetCurrentRule(curr_time);
+                    if(currentRuleModel != null){
+                    	SetRule(currentRuleModel.Rule);
+                    }
+                    
+                    
+                    next_time = GetNextRuleTime(curr_time);
+                    
+                    //Log.d(LOG_TAG, "MainService 6");
             }
             
             public void run() {
@@ -67,11 +74,16 @@ public class MainService extends Service {
                               int sub = Time.compare(curr_time, next_time);
                               if(sub > 0)
                               {
-                                      curr_rule = GetCurrentMode(curr_time);
-                                      SetRule(curr_rule);
-                                      next_time = GetNextTime(curr_time);
+                            	  	currentRuleModel = GetCurrentRule(curr_time);
+                            	  	if(currentRuleModel != null){
+                                    	SetRule(currentRuleModel.Rule);
+                                    	next_time = GetEndRuleTime(currentRuleModel);
+                                    }
+                            	  	else{
+                            	  		next_time = GetNextRuleTime(curr_time);
+                            	  	}
                               }
-                              TimeUnit.SECONDS.sleep(20);                      
+                              TimeUnit.SECONDS.sleep(30);                      
                           }
               }
               catch (InterruptedException e) {
@@ -105,55 +117,82 @@ public class MainService extends Service {
                 }
             }
             
+            /*
             private int GetRingerMode() {
                     AudioManager aManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);            
                     return aManager.getRingerMode();
             }
-          
-            private int GetCurrentMode(Time t) {
-                    int minOfDay = t.minute + t.hour * 60;
+           */
+            
+            private RuleModel GetCurrentRule(Time t) {
+                    int minOfDay = t.minute + t.hour * 60;                    
                     int day = t.weekDay;
                     int min = Integer.MAX_VALUE;
-                    TimeTable res = null;
-                    for (final TimeTable tt : ttList)
-                    {                            
-                        if(tt.day == day) {
-                                if((minOfDay - tt.getMinOfDay())+((day + 7 - tt.day)%7)*1440 < min) {
-                                        min = minOfDay - tt.getMinOfDay();
-                                        res = tt;
-                                }
-                        }                            
+                    
+                    List<Integer> rdays = new ArrayList<Integer>();                    		
+                    RuleModel res = null;
+                    
+                    for (final RuleModel tt : ttList){                   	
+                    	
+                        rdays = tt.parseDays();
+                        int rday = rdays.get(day);
+                    	
+                    	if((tt.Active == 1)&&(rday == 1)){
+	                    	if((minOfDay >= tt.StartTime)&&(minOfDay - tt.StartTime < min)){
+	                    		min = minOfDay - tt.StartTime;
+	                    		res = tt;
+	                    	}                    	
+                    	}
                     }
-                    if(res.state == 0) {
-                            return res.rule;
-                    } else {
-                            return -1;
-                    }
+                    
+                    if(min == Integer.MAX_VALUE){
+                    	if(res.EndTime - minOfDay > 0){
+                    		return res;
+                    	}
+                    } 
+                    return null;
+                    
             } 
             
-            private Time GetNextTime(Time t) {
-                    int minOfDay = t.minute + t.hour * 60;
-                    int day = t.weekDay;
-                    int min = Integer.MAX_VALUE;
-                    TimeTable res = null;
-                    
-                    for (final TimeTable tt : ttList)
-                    {                            
-                            if(tt.day == day) {
-                                    if((tt.getMinOfDay() - minOfDay)+((tt.day + 7 - day)%7)*1440 < min) {
-                                            min = tt.getMinOfDay() - minOfDay;
-                                            res = tt;
-                                    }
-                            }                            
-                    }
-                    t.weekDay = res.day;
-                    t.hour = res.hour;
-                    t.minute = res.min;
+            private Time GetNextRuleTime(Time t) {
+            	int minOfDay = t.minute + t.hour * 60;                    
+                int day = t.weekDay;
+                int min = Integer.MAX_VALUE;
+                
+                List<Integer> rdays = new ArrayList<Integer>();                    		
+                RuleModel res = null;
+                
+                for (final RuleModel tt : ttList){                   	
+                	
+                    rdays = tt.parseDays();
+                    int rday = rdays.get(day);
+                	
+                	if((tt.Active == 1)&&(rday == 1)){
+                    	if((tt.StartTime >= minOfDay)&&(tt.StartTime - minOfDay < min)){
+                    		min = tt.StartTime - minOfDay;
+                    		res = tt;
+                    	}                    	
+                	}
+                }
+                if(min != Integer.MAX_VALUE){
+                	rdays = res.parseDays();                	
+                    t.hour = res.StartTime / 60;
+                    t.minute = res.StartTime % 60;
                     return t;
+                }
+                
+                t.weekDay += 8;                
+                t.weekDay = t.weekDay % 7;                
+                return GetNextRuleTime(t);                
+            }
+            
+            private Time GetEndRuleTime(RuleModel rm) {
+            	Time t = GetCurrentTime();            	            	
+                t.hour = rm.StartTime / 60;
+                t.minute = rm.StartTime % 60;
+                return t;              
             }
             
             
-            
-            
          }
-}*/
+}
