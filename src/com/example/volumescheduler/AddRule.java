0e9d93f0973481b12e521125cd9e0e499ea0a00d
Yuriy Protskiv <iquadre@gmail.com>
2013-11-27 04:21:47 +0200

    icons.. edited addRule

--- src/com/example/volumescheduler/AddRule.java
+++ src/com/example/volumescheduler/AddRule.java
@@ -1,30 +1,28 @@
 package com.example.volumescheduler;
 
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.example.volumescheduler.AddTime.onSubmitListener;
-import android.R.string;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.DialogFragment;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TimePicker;
+import android.widget.TimePicker.OnTimeChangedListener;
 import android.widget.ToggleButton;
 
-public class AddRule extends Activity implements onSubmitListener,
-		AddDays.onSubmitListener {
+public class AddRule extends Activity {
 	final String LOG_TAG = "myLogs";
 
 	int MODE; // 1 - add, 2 - edit, ...
 
+	ListView list;
 	ToggleButton vibrate;
 	Button add_rule;
 	Button btn_addTime;
@@ -34,17 +32,15 @@ public class AddRule extends Activity implements onSubmitListener,
 	TextView tbx_days;
 
 	private int ID;
-	private int S_HOUR;
-	private int S_MIN;
-	private int E_HOUR;
-	private int E_MIN;
+	private int S_HOUR = 0;
+	private int S_MIN = 0;
+	private int E_HOUR = 0;
+	private int E_MIN = 0;
 	private int Vibrate;
 	private String Days = "";
-
-	private Map<String, Boolean> days_ = new HashMap<String, Boolean>();
-	private boolean[] days = { false, false, false, false, false, false, false };
-	private String[] days_name = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
-			"Sun" };
+	private int s_hour, s_min, e_hour, e_min;
+	
+	private String[] days_name = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
 
 	public void onCreate(Bundle savedInstanceState) {
 
@@ -64,14 +60,14 @@ public class AddRule extends Activity implements onSubmitListener,
 
 		if (MODE == 2) {
 			getParametrs();
-			if(Vibrate == 1)
+			if (Vibrate == 1)
 				tgl.setText("Checked");
-			else 
+			else
 				tgl.setText("Unchecked");
-			
+
 			btn_addDays.setText(R.string.btn_days_edit);
 			btn_addTime.setText(R.string.btn_time_edit);
-			
+
 			time.setText(S_HOUR + ":" + S_MIN + " - " + E_HOUR + ":" + E_MIN);
 			tbx_days.setText(Days);
 		}
@@ -94,14 +90,10 @@ public class AddRule extends Activity implements onSubmitListener,
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btn_addTime:
-			AddTime add_time_dlg = new AddTime();
-			add_time_dlg.mListener = AddRule.this;
-			add_time_dlg.show(getFragmentManager(), "add_time_dlg");
+			showDlgTime();
 			break;
 		case R.id.btn_addDays:
-			AddDays add_days_dlg = new AddDays();
-			add_days_dlg.mListener = AddRule.this;
-			add_days_dlg.show(getFragmentManager(), "add_days_dlg");
+			showDlgDays();
 			break;
 		case R.id.btnYes:
 			Intent intent = new Intent(this, MainActivity.class);
@@ -134,36 +126,125 @@ public class AddRule extends Activity implements onSubmitListener,
 		}
 	}
 
+	public void showDlgDays(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle("Set days");
+
+		LayoutInflater inflator = LayoutInflater.from(this);
+		View content = inflator.inflate(R.layout.activity_add_days, null);
+
+		final ListView lv = (ListView) content.findViewById(R.id.list);
+		lv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_multiple_choice,
+				days_name));
+		if(Days != "" && Days != null)
+		{
+			String[] d = Days.split(" ");
+			for (int i = 0; i < d.length; i ++) {
+				if (d[i].equals("Mon"))
+					lv.setItemChecked(0, true);
+				else if (d[i].equals("Tue"))
+					lv.setItemChecked(1, true);
+				else if (d[i].equals("Wed"))
+					lv.setItemChecked(2, true);
+				else if (d[i].equals("Thu"))
+					lv.setItemChecked(3, true);
+				else if (d[i].equals("Fri"))
+					lv.setItemChecked(4, true);
+				else if (d[i].equals("Sat"))
+					lv.setItemChecked(5, true);
+				else if (d[i].equals("Sun"))
+					lv.setItemChecked(6, true);
+			}
+		}
+		builder.setView(content).setNegativeButton("Cancel", null);
+
+	    builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+	    {
+	        @Override
+	        public void onClick(DialogInterface dialog, int whichButton)
+	        {
+	        	Days = "";
+	        	SparseBooleanArray array =  lv.getCheckedItemPositions();
+	    		//int count = lv.getCheckedItemCount();
+	    		
+	    		for(int i = 0; i < 7; i ++)
+	    		{
+	    			boolean val = array.valueAt(i);
+	    			if(array.valueAt(i))
+	    				Days += days_name[array.keyAt(i)] + " ";
+	    		}
+	    		String str = Days;
+	    		tbx_days.setText(Days);
+	        }
+	    });
+	    
+	    builder.create().show();
+	    }
+
+	public void showDlgTime(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle("Set time:");
+
+		LayoutInflater inflator = LayoutInflater.from(this);
+		View content = inflator.inflate(R.layout.activity_add_time, null);
+		
+		TimePicker timeS = (TimePicker) content.findViewById(R.id.timePickerS);
+		TimePicker timeE = (TimePicker) content.findViewById(R.id.timePickerE);
+		
+		timeS.setCurrentHour(S_HOUR);
+		timeS.setCurrentMinute(S_MIN);
+		
+		timeE.setCurrentHour(E_HOUR);
+		timeE.setCurrentMinute(E_MIN);
+		
+		timeS.setOnTimeChangedListener(new OnTimeChangedListener() {
+
+			@Override
+			public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+				s_hour = hourOfDay;
+				s_min = minute;
+
+			}
+		});
+		
+		timeE.setOnTimeChangedListener(new OnTimeChangedListener() {
+
+			@Override
+			public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+				e_hour = hourOfDay;
+				e_min = minute;
+
+			}
+		});
+		
+		builder.setView(content).setNegativeButton("Cancel", null);
+
+	    builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+	    {
+	        @Override
+	        public void onClick(DialogInterface dialog, int whichButton)
+	        {
+	        	S_HOUR = s_hour;
+	        	S_MIN = s_min;
+	        	E_HOUR = e_hour;
+	        	E_MIN = e_min;
+	        	time.setText(s_hour + ":" + s_min + " - " + e_hour + ":" + e_min);
+	        }
+	    });
+	    
+	    builder.create().show();
+	    }
+
 	public void getParametrs() {
 		ID = getIntent().getIntExtra("ID", 0);
 		S_HOUR = getIntent().getIntExtra("S_TIME", 0) / 60;
 		S_MIN = getIntent().getIntExtra("S_TIME", 0) % 60;
 		E_HOUR = getIntent().getIntExtra("E_TIME", 0) / 60;
-		S_MIN = getIntent().getIntExtra("E_TIME", 0) % 60;
+		E_MIN = getIntent().getIntExtra("E_TIME", 0) % 60;
 		Vibrate = getIntent().getIntExtra("Vibrate", 0);
 		Days = getIntent().getStringExtra("Days");
 	}
-
-	public void setOnSubmitListener(int s_hour, int s_min, int e_hour, int e_min) {
-		S_HOUR = s_hour;
-		S_MIN = s_min;
-		E_HOUR = e_hour;
-		E_MIN = e_min;
-		time.setText(s_hour + ":" + s_min + " - " + e_hour + ":" + e_min);
-	}
-
-	public void setOnSubmitListener(boolean[] days) {
-		// this.days = days;
-
-		for (int i = 0; i < days.length; i++) {
-			days_.put(days_name[i], days[i]);
-		}
-		Days = "";
-		for (int i = 0; i < days_.size(); i++) {
-			if (days_.get(days_name[i]))
-				Days += days_name[i] + " ";
-		}
-		tbx_days.setText(Days);
-	}
-
 }
